3bca4bfa2f8afe29b6ebf8fae9a97cedbcb472c5
Artem Makovskyy <photomak79@gmail.com>
2026-04-25 23:46:53 +0300

    fix: rename products table to product per PLAN.md schema

--- src/main/resources/db/changelog/common/2026/04/V001__create_products_table.sql
+++ src/main/resources/db/changelog/common/2026/04/V001__create_products_table.sql
@@ -2,17 +2,13 @@
 -- logicalFilePath: db/changelog/common/2026/04/V001__create_products_table.sql
 
 -- changeset author:1
-CREATE TABLE products (
-    id BIGSERIAL PRIMARY KEY,
-    item_id INTEGER UNIQUE NOT NULL,
-    name VARCHAR(255) NOT NULL,
+CREATE TABLE product (
+    item_id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
     category VARCHAR(100),
-    price DECIMAL(10, 2) NOT NULL,
+    price DECIMAL(10, 2),
     short_description TEXT
 );
 
 -- changeset author:2
-CREATE INDEX idx_products_category ON products(category);
-
--- changeset author:3
-CREATE INDEX idx_products_item_id ON products(item_id);
+CREATE INDEX idx_products_category ON product(category);
